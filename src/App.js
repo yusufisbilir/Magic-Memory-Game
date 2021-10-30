56be5b694791530a1940639c9b667ee36eb64371
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-10-30 17:15:21 +0300

    Add images. Shuffle cards.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React, { useState } from "react";
 import "./App.sass";
 
+const cardImg = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+
 function App() {
-  return <div className="App"></div>;
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  //shuffle
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImg, ...cardImg]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
+  return (
+    <div className="App">
+      <h1>Magic Memory Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+    </div>
+  );
 }
 
 export default App;
