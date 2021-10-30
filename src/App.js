474cfada332bb763ecc6ee7d200c499c762ccdac
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-10-30 19:04:54 +0300

    Flip cards

--- src/App.js
+++ src/App.js
@@ -45,9 +45,10 @@ function App() {
             }
           });
         });
+
         resetTurn();
       } else {
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
   }, [choiceOne, choiceTwo]);
@@ -66,7 +67,12 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./SingleCard.sass";
 
-function SingleCard({ card, handleChoice }) {
+function SingleCard({ card, handleChoice, flipped }) {
   const handleClick = () => {
     handleChoice(card);
   };
 
   return (
     <div className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card" />
         <img
           className="back"
